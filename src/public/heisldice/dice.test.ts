29b074edad72ba3b9951b480d3cb873edb2794ac
Ryan Heisler <info@ryanheisler.com>
2025-06-14 09:50:26 -0400

    Make dice inaccessible while rolling

--- src/public/heisldice/dice.test.ts
+++ src/public/heisldice/dice.test.ts
@@ -7,10 +7,10 @@ import { mockServer } from "../../../test/mock-server/mock-server";
 import { getGeneratorFromArray } from "../../../test/generator";
 
 describe("dice", () => {
+    let screen: Container;
+    let rollButton: HTMLButtonElement;
     describe("when the dice are rolled", () => {
         let diceRolls: number[][];
-        let screen: Container;
-        let rollButton: HTMLButtonElement;
         beforeEach(async () => {
             diceRolls = [
                 [4, 4, 3, 1, 2],
@@ -29,7 +29,7 @@ describe("dice", () => {
         });
         it("updates the list of dice", async () => {
             await screen.user.click(rollButton);
-            const dice = within(screen.getByRole("list")).getAllByRole("listitem");
+            const dice = within(await screen.findByRole("list")).getAllByRole("listitem");
             const firstRoll = diceRolls[0];
 
             expect(dice[0].textContent).toBe(`${firstRoll[0]}`);
@@ -55,6 +55,30 @@ describe("dice", () => {
 
             expect(entries[0].textContent).toEqual(`You rolled ${secondRoll[0]}, ${secondRoll[1]}, ${secondRoll[2]}, ${secondRoll[3]}, ${secondRoll[4]}`);
             expect(entries[1].textContent).toEqual(`You rolled ${firstRoll[0]}, ${firstRoll[1]}, ${firstRoll[2]}, ${firstRoll[3]}, ${firstRoll[4]}`);
+        });
+    });
+
+    describe("while the dice are rolling", () => {
+        let resolve: () => void;
+        beforeEach(async () => {
+            mockServer.use(http.get(`*${heisldiceRoutes.dice}`, async () => {
+                await new Promise<void>(resolve_ => {resolve = resolve_;});
+                return HttpResponse.json({ dice: [1, 2, 3, 4, 5] });
+            }));
+
+            screen = await render("heisldice.html");
+
+            rollButton = screen.getByRole("button", { name: /roll dice/i });
+            await screen.user.click(rollButton);
+        });
+        it("dice elements are inaccessible to assistive technology", async () => {
+            let dice = screen.queryByRole("list");
+            expect(dice).not.toBeInTheDocument();
+
+            resolve();
+
+            dice = await screen.findByRole("list");
+            expect(dice).toBeInTheDocument();
         });
     });
 });
--- src/public/heisldice/dice.ts
+++ src/public/heisldice/dice.ts
@@ -1,10 +1,11 @@
 const diceRollButtonId = "dice-roll-button";
+const diceContainerId = "dice-container";
 
 const rollDice = async (): Promise<number[]> => {
     const response = await fetch(`/dice`, { method: "GET" });
     const { dice } = await response.json();
 
-    const diceElements = document.getElementById("dice-container")?.querySelectorAll("li");
+    const diceElements = document.getElementById(diceContainerId)?.querySelectorAll("li");
 
     diceElements?.forEach((element, index) => {
         element.textContent = `${dice[index]}`;
@@ -26,9 +27,14 @@ const addLogEntry = (message: string) => {
 const button = document.getElementById(diceRollButtonId);
 
 button?.addEventListener("click", async (event: MouseEvent) => {
+    const diceContainer = document.getElementById(diceContainerId);
+    diceContainer?.setAttribute("aria-hidden", "true");
+
     const button = event.target as HTMLButtonElement;
     if (button && button.id === diceRollButtonId) {
         const dice = await rollDice();
+
+        diceContainer?.setAttribute("aria-hidden", "false");
 
         let message = `You rolled ${dice[0]}, ${dice[1]}, ${dice[2]}, ${dice[3]}, ${dice[4]}`;
         addLogEntry(message);
